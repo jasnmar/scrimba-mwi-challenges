967c988b3903cfb440ab83f976dd3fa70297e7a6
jasnmar <jasnmar@gmail.com>
2024-03-28 16:04:06 -0400

    challenge 4

--- index.js
+++ index.js
@@ -1,22 +1,7 @@
-let myPoints = 3
-
-// Create two functions, add3Points() and remove1Point(), and have them
-// add/remove points to/from the myPoints variable
-
-function add3Points() {
-    myPoints += 3;
-}
-function remove1Point() {
-    myPoints--;
-}
-add3Points();
-add3Points();
-add3Points();
-remove1Point();
-remove1Point();
-
-
-
-
-// Call the functions to that the line below logs out 10
-console.log(myPoints)
+// Try to predict what each of the lines will log out
+console.log("2" + 2) // 22
+console.log(11 + 7) // 18
+console.log(6 + "5") // 65
+console.log("My points: " + 5 + 9) // My points59
+console.log(2 + 2) //  4
+console.log("11" + "14") // 1114 
